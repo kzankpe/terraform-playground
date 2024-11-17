1aab8f7fdcdbd6c949a82ab12065d007a325ac5c
kzankpe <78028172+kzankpe@users.noreply.github.com>
2024-11-17 21:04:24 +0000

    chore: add initial template for code

---
+++ containerapp/providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    azurerm = {
+        source="hashicorp/azurerm"
+        version="=4.10.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+  use_msi = true
+}
---
+++ containerapp/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  type        = string
+  description = "Resource location"
+}
+
+variable "rgname" {
+  type        = string
+  description = "Resource group name"
+}
